34857c76f467c5c015bb04e065609af6a5abc386
Daniel Nagornov <danieln056@gmail.com>
2023-04-16 15:31:02 +0300

    disable console from showing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "gui.hpp"
 
-//#define _DEBUG_
+// #define _DEBUG_
 
-int main(int, char**) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
     procList = getProcList();
 
 #ifdef _DEBUG_
@@ -10,7 +11,8 @@ int main(int, char**) {
 #endif
 
     createAppWindow();
-    if (!initializeDirect3D()) return 1;
+    if (!initializeDirect3D())
+        return 1;
     showWindow();
     setupImguiContext();
     ImGui::StyleColorsLight();
@@ -21,9 +23,11 @@ int main(int, char**) {
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
-    while (!done) {
+    while (!done)
+    {
         MSG msg;
-        while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+        while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+        {
             ::TranslateMessage(&msg);
             ::DispatchMessage(&msg);
             if (msg.message == WM_QUIT)
@@ -35,7 +39,7 @@ int main(int, char**) {
         startNewFrame();
         app();
 
-        const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
+        const float clear_color_with_alpha[4] = {clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w};
         render(clear_color_with_alpha);
         g_pSwapChain->Present(1, 0);
     }
